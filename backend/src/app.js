2f5263ca4f5b8af0e8064045f30498e47edda2dc
Hardik Gupta <guptahardik666@gmail.com>
2025-12-18 01:08:34 +0530

    feat: Installed and resolved CORS for frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const authController = require("./controllers/auth.controller");
 const sweetsController = require("./controllers/sweets.controller");
 const authenticate = require("./middleware/auth.middleware");
 
 const app = express();
+
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 // auth
 app.post("/api/auth/register", authController.register);
